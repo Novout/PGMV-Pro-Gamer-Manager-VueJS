c6123369183f8883f46f41a1ee59b1906605e3d6
Novout <novout@hotmail.com>
2020-08-01 04:06:44 -0300

    chore(icons): added icons structure

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import router from "./router";
 import store from "./store";
 import "./css/main.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
