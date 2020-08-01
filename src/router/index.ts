8179533fab6d3fbdf3c81fc3643466377570e703
Novout <novout@hotmail.com>
2020-07-31 21:37:47 -0300

    chore: structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,7 +21,7 @@ const routes: Array<RouteConfig> = [
   }
 ];
 
-const router = new VueRouter({
+const router: VueRouter = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
