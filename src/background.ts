aa75cf5d96c21d0d3de0b18ba3138c958941652f
Novout <novout@hotmail.com>
2020-08-18 06:45:31 -0300

    chore(toml): base env config

--- src/background.ts
+++ src/background.ts
@@ -1,22 +1,24 @@
 "use strict";
 
+require("dotenv").config();
 import { app, protocol, BrowserWindow, globalShortcut } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 // @ts-ignore
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
-const isDevelopment = process.env.NODE_ENV !== "production";
+import { readFileSync } from "fs";
+import { parse } from "@iarna/toml";
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const { PRODUCTION_MODE } = parse(
+  readFileSync("./env.toml", { encoding: "utf-8" })
+);
+
 let win: BrowserWindow | undefined | null;
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } }
 ]);
 
 const createWindow = () => {
-  // Create the browser window.
   win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -30,11 +32,9 @@ const createWindow = () => {
   });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
   } else {
     createProtocol("app");
-    // Load the index.html when not in development
     win.loadURL("app://./index.html");
   }
 
@@ -45,7 +45,9 @@ const createWindow = () => {
     win.setTitle("PGMJ - Pro Gamer Manager VueJS");
   });
 
-  win.setFullScreen(true);
+  if (PRODUCTION_MODE) {
+    win.setFullScreen(true);
+  }
 
   win.on("closed", () => {
     win = null;
@@ -61,28 +63,20 @@ const createShortcuts = () => {
 
 app.on("ready", createShortcuts);
 
-// Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow();
   }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
+  if (PRODUCTION_MODE && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
       await installExtension(VUEJS_DEVTOOLS);
@@ -93,8 +87,7 @@ app.on("ready", async () => {
   createWindow();
 });
 
-// Exit cleanly on request from parent process in development mode.
-if (isDevelopment) {
+if (PRODUCTION_MODE) {
   if (process.platform === "win32") {
     process.on("message", data => {
       if (data === "graceful-exit") {
