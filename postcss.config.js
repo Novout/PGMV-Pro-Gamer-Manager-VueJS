3a2100fd8edd7fc03cda23904b5671585ab33f54
Novout <novout@hotmail.com>
2020-08-21 01:05:16 -0300

    chore(postcss): added plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,33 @@
+import { envService } from "@/services/defines/env";
+
+const { PRODUCTION_MODE } = envService.getBaseEnv();
+
 module.exports = {
-  plugins: [require("postcss-import"), require("autoprefixer")]
+  plugins: [
+    PRODUCTION_MODE &&
+      require("@fullhuman/postcss-purgecss")({
+        content: [`./public/**/*.html`, `./src/**/*.vue`],
+        defaultExtractor(content) {
+          const contentWithoutStyleBlocks = content.replace(
+            /<style[^]+?<\/style>/gi,
+            ""
+          );
+          return (
+            contentWithoutStyleBlocks.match(
+              /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+            ) || []
+          );
+        },
+        whitelist: [],
+        whitelistPatterns: [
+          /-(leave|enter|appear)(|-(to|from|active))$/,
+          /^(?!(|.*?:)cursor-move).+-move$/,
+          /^router-link(|-exact)-active$/,
+          /data-v-.*/
+        ]
+      }),
+    require("autoprefixer"),
+    require("postcss-import"),
+    require("postcss-utilities")
+  ]
 };
